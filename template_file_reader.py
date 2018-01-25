a7a8c710b11486fabf13629409aa2ff15b1c8e7d
Lewis Kelley <kelleyld@rose-hulman.edu>
2018-01-24 20:23:56 -0500

    Added templates and way to read them into the main program. (#11)
    Close #3 
    
    * Added templates and way to read them into the main program.
    
    * Removed unnecessary declarations from template_file_reader.

---
+++ template_file_reader.py
@@ -0,0 +1,19 @@
+import random
+
+with open('flip_templates.txt') as flip_templates_file:
+    _flip_templates = flip_templates_file.readlines()
+
+with open('move_templates.txt') as move_templates_file:
+    _move_templates = move_templates_file.readlines()
+
+def read_flip_template(color, letter):
+    return _read_template(_flip_templates, color, letter)
+
+def read_move_template(color, letter):
+    return _read_template(_move_templates, color, letter)
+
+def _read_template(templates, color, letter):
+    rand_template = random.choice(templates)
+    return rand_template.replace('%c', color)  \
+                        .replace('%l', letter) \
+                        .replace('\n', '')
