bf824d2b13ae6a1752dc0f6d609c9a024a4ab7b1
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-02-07 16:49:37 -0500

    Added -r flag to pipeline script. Renamed arguments to be consistent

--- build_neural_network.sh
+++ build_neural_network.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if (( $# != 3))
+if (( $# < 3))
 then
     echo $#
-    echo "Usage: $0 <num_games> <num_blocks> <model-h5-file>"
+    echo "Usage: $0 <num-games> <num-blocks> <model-h5-file> (-r)"
     exit 1
 fi
 
@@ -16,6 +16,13 @@ H5_FILE=$3
 
 python3 generate_instructions.py $NUM_GAMES $NUM_BLOCKS $DATA_FILE
 python3 split_file.py $NUM_BLOCKS $DATA_FILE $NEURAL_IN_FILE $NEURAL_OUT_FILE
-rm -f $DATA_FILE
 python3 hyperparameter_train_network.py $NUM_BLOCKS $NEURAL_IN_FILE $NEURAL_OUT_FILE $H5_FILE
-rm -f $NEURAL_IN_FILE $NEURAL_OUT_FILE
+
+if (( $# > 3 ))
+then
+    rm -f $DATA_FILE $NEURAL_IN_FILE $NEURAL_OUT_FILE
+else
+    mv $DATA_FILE data.csv
+    mv $NEURAL_IN_FILE neural_in.csv
+    mv $NEURAL_OUT_FILE neural_out.csv
+fi
