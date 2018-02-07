f40c53c1cb170ad230733a29a330acd2e040e3bb
Coleman Gibson <gibsonjc@rose-hulman.edu>
2018-02-06 19:07:17 -0500

    Flattened nested 'with's

--- split_file.py
+++ split_file.py
@@ -2,25 +2,25 @@ import csv
 import sys
 
 def split_file(num_blocks, input_file_name, output_neural_in, output_neural_out):
-    with open(input_file_name, 'r') as csv_in_file:
-        with open(output_neural_in, 'w') as csv_neural_in_file:
-            with open(output_neural_out, 'w') as csv_neural_out_file:
-                reader = csv.reader(csv_in_file)
-                neural_in_writer = csv.writer(csv_neural_in_file)
-                neural_out_writer = csv.writer(csv_neural_out_file)
+    with open(input_file_name, 'r') as csv_in_file, \
+         open(output_neural_in, 'w') as csv_neural_in_file, \
+         open(output_neural_out, 'w') as csv_neural_out_file:
+        reader = csv.reader(csv_in_file)
+        neural_in_writer = csv.writer(csv_neural_in_file)
+        neural_out_writer = csv.writer(csv_neural_out_file)
 
-                # remove headers
-                row = reader.__next__()
+        # remove headers
+        row = reader.__next__()
 
-                for row in reader:
-                    state_cols = row[:num_blocks * 7]
-                    word_col = row[-3]
-                    input_cols = state_cols + [word_col]
-                    output_cols = [0] * num_blocks
-                    output_cols[int(row[-6]) - 1] = 1
+        for row in reader:
+            state_cols = row[:num_blocks * 7]
+            word_col = row[-3]
+            input_cols = state_cols + [word_col]
+            output_cols = [0] * num_blocks
+            output_cols[int(row[-6]) - 1] = 1
 
-                    neural_in_writer.writerow(input_cols)
-                    neural_out_writer.writerow(output_cols)
+            neural_in_writer.writerow(input_cols)
+            neural_out_writer.writerow(output_cols)
 
 def main():
     if len(sys.argv) != 5:
