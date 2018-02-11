1b2685d365a6fbf577687c2c3598e401736c7fbe
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-02-11 09:31:03 -0500

    Added double blocks to the templates.
    e.g. "Move the red block next to the green one"

--- template_file_reader.py
+++ template_file_reader.py
@@ -6,14 +6,18 @@ with open('flip_templates.txt') as flip_templates_file:
 with open('move_templates.txt') as move_templates_file:
     _move_templates = move_templates_file.readlines()
 
-def read_flip_template(color, letter):
-    return _read_template(_flip_templates, color, letter)
+def read_flip_template(color, letter, other_color=None, other_letter=None):
+    return _read_template(_flip_templates, color, letter, other_color, other_letter)
 
-def read_move_template(color, letter):
-    return _read_template(_move_templates, color, letter)
+def read_move_template(color, letter, other_color=None, other_letter=None):
+    return _read_template(_move_templates, color, letter, other_color, other_letter)
 
-def _read_template(templates, color, letter):
+def _read_template(templates, color, letter, other_color, other_letter):
     rand_template = random.choice(templates)
+
+    if ((not other_color and '%C' in rand_template)
+        or (not other_letter and '%L' in rand_template)):
+        return _read_template(templates, color, letter, other_color, other_letter)
 
     result = {}
 
@@ -21,5 +25,7 @@ def _read_template(templates, color, letter):
     result['letter'] = '%l' in rand_template
     result['text'] = rand_template.replace('%c', color)  \
                                   .replace('%l', letter) \
+                                  .replace('%C', other_color) \
+                                  .replace('%L', other_letter) \
                                   .replace('\n', '')
     return result
